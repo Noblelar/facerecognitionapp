7a6ffccc6795907b0334fff79f184fe072a6443e
Noblelar <59696875+Noblelar@users.noreply.github.com>
2023-05-26 18:53:39 +0000

    fra

---
+++ src/component/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import './logo.css'
+import brain from '../../container/brain.png'
+
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br3 shadow-2" options={{ max: 55 }}>
+                <div className="Tilt-inner">
+                    <img src={brain} alt="Logo" />
+                </div>
+            </Tilt>
+        </div>
+    )
+}
+
+
+export default Logo;
---
+++ src/component/navigation/navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Navigation  = () => {
+    return(
+        <nav style={{display: 'flex', justifyContent: "right"}}>
+            <p> Sign Out </p>
+        </nav>
+    )
+
+}
+
+
+export default Navigation;
